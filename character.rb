db40e6fd113334f07ad64e5045d102c017f5ea2e
BenBart2514 <b.bartholomew2514@gmail.com>
2021-09-23 14:48:32 +1000

    created character variables and started building main menu

--- character.rb
+++ character.rb
@@ -1,5 +1,64 @@
 class Character
-    def initialize(name)
-        @name = name
-    end
+  attr_reader :name, :race, :hp, :max_hp, :mana, :max_mana, :stamina, :max_stamina, :ap, :max_ap, :attack, :defence
+
+  @@characters = []
+  @@character_count = @@characters.length
+
+  def initialize(name, race = 'Human')
+    @name = name.capitalize
+    @race = race.capitalize
+    @@characters << self
+  end
+
+  def change_name
+    puts "What would you like to change #{name}'s name to?"
+    new_name = gets.chomp.capitalize
+    @name = new_name
+  end
+
+  def change_race
+    puts "What would you like to change #{name}'s race to?"
+    new_race = gets.chomp.capitalize
+    @race = new_race
+  end
+
+  def set_hp
+    puts "Enter #{name}'s HP: "
+    hp = gets.chomp.to_i
+    @max_hp = hp
+    @hp = hp
+  end
+
+  def set_mana
+    puts "Enter #{name}'s Mana: "
+    mana = gets.chomp.to_i
+    @max_mana = mana
+    @mana = mana
+  end
+
+  def set_stamina
+    puts "Enter #{name}'s Stamina: "
+    stamina = gets.chomp.to_i
+    @max_stamina = stamina
+    @stamina = stamina
+  end
+
+  def set_ap
+    puts "Enter #{name}'s AP: "
+    ap = gets.chomp.to_i
+    @max_ap = ap
+    @ap = ap
+  end
+
+  def set_attack
+    puts "Enter #{name}'s Attack: "
+    attack = gets.chomp.to_i
+    @attack = attack
+  end
+
+  def set_defence
+    puts "Enter #{name}'s Defence: "
+    defence = gets.chomp.to_i
+    @defence = defence
+  end
 end
--- tests/character_test.rb
+++ tests/character_test.rb
@@ -3,10 +3,62 @@ require_relative '../character'
 
 class CharacterTest < Test::Unit::TestCase
   def setup
-    @character = Character.new('Aragorn')
+    @character = Character.new('aragorn')
   end
 
   def test_create
     assert_not_nil(@character)
+  end
+
+  def test_name
+    assert_equal('Aragorn', @character.name)
+  end
+
+  def test_change_name
+    input = @character.change_name
+    assert_equal(input, @character.name)
+  end
+
+  def test_race
+    assert_not_nil(@character.race)
+  end
+
+  def test_race_change
+    input = @character.change_race
+    assert_equal(input, @character.race)
+  end
+
+  def test_hp
+    input = @character.set_hp
+    assert_equal(input, @character.max_hp)
+    assert_equal(input, @character.hp)
+  end
+
+  def test_mana
+    input = @character.set_mana
+    assert_equal(input, @character.max_mana)
+    assert_equal(input, @character.mana)
+  end
+
+  def test_stamina
+    input = @character.set_stamina
+    assert_equal(input, @character.max_stamina)
+    assert_equal(input, @character.stamina)
+  end
+
+  def test_ap
+    input = @character.set_ap
+    assert_equal(input, @character.max_ap)
+    assert_equal(input, @character.ap)
+  end
+
+  def test_attack
+    input = @character.set_attack
+    assert_equal(input, @character.attack)
+  end
+
+  def test_defence
+    input = @character.set_defence
+    assert_equal(input, @character.defence)
   end
 end
